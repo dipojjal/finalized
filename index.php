5ce3357f3786c88aaac66ce0ec7c2f5ec26ee793
dipojjal <cooldipojjal@gmail.com>
2017-07-05 21:00:06 +0530

    login or signup

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
             <div class="intro-text">
                 <div class="intro-lead-in">Wipro Product Management System</div>
                 <div class="intro-heading"></div>
-                <a href="login.html" class="btn btn-xl">Login</a>
+                <a href="login.html" class="btn btn-xl">Login/Signup</a>
             </div>
         </div>
     </header>
