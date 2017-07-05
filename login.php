566067f5154db8469a1ad7652c4418082b30ed89
dipojjal <cooldipojjal@gmail.com>
2017-07-05 23:48:15 +0530

    login php

--- login.php
+++ login.php
@@ -65,21 +65,21 @@ echo'Username or password is wrong';
 
 }
 
-echo'<form name=\"login_form\" method=\"POST\" action=\"\">
+echo'<form name=\"login_form\" method=\"POST\" action=\"\">';
   
-  <fieldset>
-    <img src=\"Wipro_Logo_RGB.png\" height=\"100px\" width=\"150px\"></br></br>
-    <h1 class=\"fs-heading\">Login to your account</h1></br>
-    <input type=\"text\" name=\"username\" placeholder=\"Username\"/><br/>
-    <input type=\"password\" name=\"password\" placeholder=\"Password\" /><br/>
-    <input type=\"submit\" name=\"login\" class=\"next action-button\" value=\"Submit\" /> <br/> <br/>
-    <hr>
-    <br/> 
-    <h3 class=\"fs-title\" >New User? <a href=\"signup.html\">Sign Up</a></h3>
+  echo'<fieldset>';
+    echo'<img src=\"Wipro_Logo_RGB.png\" height=\"100px\" width=\"150px\"></br></br>';
+    echo'<h1 class=\"fs-heading\">Login to your account</h1></br>';
+    echo'<input type=\"text\" name=\"username\" placeholder=\"Username\"/><br/>';
+    echo'<input type=\"password\" name=\"password\" placeholder=\"Password\" /><br/>';
+    echo'<input type=\"submit\" name=\"login\" class=\"next action-button\" value=\"Submit\" /> <br/> <br/>';
+    echo'<hr>';
+    echo'<br/>'; 
+    echo'<h3 class=\"fs-title\" >New User? <a href=\"signup.html\">Sign Up</a></h3>';
  
- </fieldset>
+ echo'</fieldset>';
 
-</form>
+echo'</form>';
 ?>
 </body>
 </html>
