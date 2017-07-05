44d10b77e87bcc9da58f30755be3fb644b93394e
dipojjal <cooldipojjal@gmail.com>
2017-07-05 23:45:10 +0530

    login php

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<html>
+	<head><title>Login- Wipro Product Management Sysem</title>
+	<link rel = "stylesheet" type = "text/css" href = "myfile.css" />
+	</head>
+	
+	<body>
+	
 <?php
 
 session_start();
@@ -58,19 +65,21 @@ echo'Username or password is wrong';
 
 }
 
-<form name="login_form" method="POST" action="">
+echo'<form name=\"login_form\" method=\"POST\" action=\"\">
   
   <fieldset>
-    <img src="Wipro_Logo_RGB.png" height="100px" width="150px"></br></br>
-    <h1 class="fs-heading" >Login to your account</h1></br>
-    <input type="text" name="username" placeholder=" Username"/><br/>
-    <input type="password" name="password" placeholder="Password" /><br/>
-    <input type="submit" name="login" class="next action-button" value="Submit" /> <br/> <br/>
+    <img src=\"Wipro_Logo_RGB.png\" height=\"100px\" width=\"150px\"></br></br>
+    <h1 class=\"fs-heading\">Login to your account</h1></br>
+    <input type=\"text\" name=\"username\" placeholder=\"Username\"/><br/>
+    <input type=\"password\" name=\"password\" placeholder=\"Password\" /><br/>
+    <input type=\"submit\" name=\"login\" class=\"next action-button\" value=\"Submit\" /> <br/> <br/>
     <hr>
     <br/> 
-    <h3 class="fs-title" >New User? <a href="signup.html">Sign Up</a></h3>
+    <h3 class=\"fs-title\" >New User? <a href=\"signup.html\">Sign Up</a></h3>
  
  </fieldset>
 
 </form>
 ?>
+</body>
+</html>
