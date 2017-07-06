ed8c9e3d5d1af8831a7637eef8ec4490280134bc
dipojjal <cooldipojjal@gmail.com>
2017-07-06 14:01:05 +0530

    loginProcess.php

--- login.php
+++ login.php
@@ -5,17 +5,18 @@
 	
 	<body>
 	
-<form name=\"login_form\" method=\"POST\" action=\"\">
+<form name="login_form" method="POST" action="loginProcess.php">
   
   <fieldset>
-    <img src=\"Wipro_Logo_RGB.png\" height=\"100px\" width=\"150px\"></br></br>
-    <h1 class=\"fs-heading\">Login to your account</h1></br>
-    <input type=\"text\" name=\"username\" placeholder=\"Username\"/><br/>
-    <input type=\"password\" name=\"password\" placeholder=\"Password\" /><br/>
-    <input type=\"submit\" name=\"login\" class=\"next action-button\" value=\"Submit\" /> <br/> <br/>
+    <img src="Wipro_Logo_RGB.png" height="100px" width="150px"></br></br>
+    <h1 class="fs-heading">Login to your account</h1></br>
+    <input type="text" name="username" placeholder="Username" /><br/>
+    <input type="password" name="password" placeholder="Password" /><br/>
+	
+    <input type="submit" name="login" class="next action-button" value="Submit" /> <br/> <br/>
     <hr>
     <br/>
-    <h3 class=\"fs-title\" >New User? <a href=\"signup.html\">Sign Up</a></h3>
+    <h3 class="fs-title" >New User? <a href="signup.html">Sign Up</a></h3>
  
  </fieldset>
 
