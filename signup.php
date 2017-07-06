29383e4ffad494808d7c7bb38b1fded88ca81a7c
dipojjal <cooldipojjal@gmail.com>
2017-07-06 21:40:51 +0530

    post to createUser.php

--- signup.php
+++ signup.php
@@ -4,14 +4,14 @@
 </head>
 <body><!-- multistep form -->
 
-<form id="msform">
+<form id="msform" method="POST" action="createUser.php">
   
   <fieldset>
     <img src="Wipro_Logo_RGB.png" height="100px" width="150px"></br></br>
     <h1 class="fs-heading" >Sign up</h1></br>
-    <input type="text" name="First Name" placeholder=" Enter your first name"/><br/>
-    <input type="text" name="Last Name" placeholder="Enter your last name" /><br/>
-    <input type="text" name="Email" placeholder="Enter your email address" /><br/>
+    <input type="text" name="First Name" placeholder=" Enter your first name" required/><br/>
+    <input type="text" name="Last Name" placeholder="Enter your last name" required/><br/>
+    <input type="text" name="Email" placeholder="Enter your email address" required/><br/>
     <textarea name="About" placeholder="About your self" ></textarea><br/>
     <input type="button" name="Login" class="next action-button" value="submit" /> <br/> <br/>
     <hr>
