78f6572072d1daf6c662ed3e5d5781d7d8359492
dipojjal <cooldipojjal@gmail.com>
2017-07-06 14:25:43 +0530

    loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -2,17 +2,7 @@
 
 session_start();
 
-if(isset($_SESSION['user_email'])){
 
-header('location: index.php');
-
-}
-
-else{
-
-}
-
-if(isset($_POST['login'])){
 
 	$username= $_POST['username'];
 	$password= $_POST['password'];
@@ -35,12 +25,6 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 $result = pg_query($pg_conn, "SELECT email__c, password__c FROM salesforce.Heroku_User__c where email__c= '$username' AND password__c= '$password' ");
 
-if (!$result) {
-  echo "An error occurred.\n";
-  header('location: index.php');
-  exit;
-}
-
 $rows = pg_num_rows($result);
 
 if($rows > 0){
@@ -54,8 +38,6 @@ header('location: mainPage.php')
 
 else{
 echo'Username or password is wrong';
-}
-
 }
 
 }
