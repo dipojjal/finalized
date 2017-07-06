d1f6cacbf4ddf384b1dc5503dbbd52b5949aebf1
dipojjal <cooldipojjal@gmail.com>
2017-07-06 14:01:34 +0530

    loginProcess

---
+++ loginProcess.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION['user_email'])){
+
+header('location: index.php');
+
+}
+
+else{
+
+}
+
+if(isset($_POST['login'])){
+
+	$username= $_POST['username'];
+	$password= $_POST['password'];
+
+if(empty($username) || empty($password)){
+
+echo "Please enter values in username and password fields";
+
+}
+
+else{
+
+  $password= sha1($password);
+
+	function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+}
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$result = pg_query($pg_conn, "SELECT email__c, password__c FROM salesforce.Heroku_User__c where email__c= '$username' AND password__c= '$password' ");
+
+if (!$result) {
+  echo "An error occurred.\n";
+  header('location: index.php');
+  exit;
+}
+
+$rows = pg_num_rows($result);
+
+if($rows > 0){
+
+$_SESSION['is_logged_in']= true;
+$_SESSION['user_email']= $username;
+
+header('location: mainPage.php')
+
+}
+
+else{
+echo'Username or password is wrong';
+}
+
+}
+
+}
