32b843fd5febac84b7b331041949e4f553c6a644
dipojjal <cooldipojjal@gmail.com>
2017-07-08 00:50:20 +0530

    signup

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@
     <h1 class="fs-heading" >Sign up</h1></br>
     <input type="text" name="name" placeholder="Enter your full name" required/><br/>
     <input type="text" name="email" placeholder="Enter your email address" required/><br/>
-    <input type="text" name="password" placeholder="Enter your email address" required/><br/>
+    <input type="text" name="password" placeholder="Enter your password" required/><br/>
     <textarea name="about" placeholder="About your self" ></textarea><br/>
     <input type="button" name="Login" class="next action-button" value="submit" /> <br/> <br/>
     <hr>
