30c48bd01940b5eb1747967b91f5e863d39fd1c5
dipojjal <cooldipojjal@gmail.com>
2017-07-08 00:05:00 +0530

    signup

--- signup.php
+++ signup.php
@@ -9,10 +9,10 @@
   <fieldset>
     <img src="Wipro_Logo_RGB.png" height="100px" width="150px"></br></br>
     <h1 class="fs-heading" >Sign up</h1></br>
-    <input type="text" name="Name" placeholder="Enter your full name" required/><br/>
-    <input type="text" name="Email" placeholder="Enter your email address" required/><br/>
-    <input type="text" name="Password" placeholder="Enter your email address" required/><br/>
-    <textarea name="About" placeholder="About your self" ></textarea><br/>
+    <input type="text" name="name" placeholder="Enter your full name" required/><br/>
+    <input type="text" name="email" placeholder="Enter your email address" required/><br/>
+    <input type="text" name="password" placeholder="Enter your email address" required/><br/>
+    <textarea name="about" placeholder="About your self" ></textarea><br/>
     <input type="button" name="Login" class="next action-button" value="submit" /> <br/> <br/>
     <hr>
     <br/> 
