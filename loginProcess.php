1f809df04981140949de10f645124a38677f0159
dipojjal <cooldipojjal@gmail.com>
2017-07-07 23:11:38 +0530

    loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -24,17 +24,18 @@ else{
 	
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $pwd= sha1($password);
-$result = pg_query($pg_conn, "SELECT username__c, password__c FROM salesforce.Heroku_User__c where username__c= '$username' ");
+$result = pg_query($pg_conn, "SELECT username__c, Pass__c FROM salesforce.Heroku_User__c where username__c= '$username' Limit 1");
 
 $rows = pg_num_rows($result);
 
 if($rows > 0){
-
+	if($row[1]=$pwd){
 $_SESSION['is_logged_in']= true;
 $_SESSION['user_email']= $username;
 
 header('location: mainPage.php');
-
+	}
+	
 }
 
 else{
