fed33b6de44908e7e078c40980c2502f3009c88e
dipojjal <cooldipojjal@gmail.com>
2017-07-10 18:13:46 +0530

    mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -883,7 +883,7 @@
                                 <!-- Project Details Go Here -->
                                 
                                 <div class=\"panel-heading\"><center><h2>All Products</h2></center></div>
-
+<div style="clear:both; font-size=60px">Hello, php starts here</div>
 <?php
 				    
 function pg_connection_string_from_database_url() {
@@ -937,29 +937,30 @@ echo '</div>';
 echo '</div>';
 echo '</div>';
 ?>
-<!-- Modal 6 -->;
-<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">;
-<div class="modal-dialog">;
-<div class="modal-content">;
-<div class="close-modal" data-dismiss="modal">;
-<div class="lr">;
-<div class="rl">;
-</div>;
-</div>;
-</div>;
-<div class="container">;
-<div class="row">;
-<div class="col-lg-8 offset-lg-2">;
-<div class="modal-body">;
-<!-- Project Details Go Here -->;
-<h2>Product Catalog</h2>;
-</div>;
-</div>;
-</div>;
-</div>;
-</div>;
-</div>;
-</div>;
+				    <div style="clear:both; font-size=60px">Hello, php ends here</div>
+<!-- Modal 6 -->
+<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="close-modal" data-dismiss="modal">
+<div class="lr">
+<div class="rl">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-8 offset-lg-2">
+<div class="modal-body">
+<!-- Project Details Go Here -->
+<h2>Product Catalog</h2>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 <!-- Custom scripts for this template -->
 <script src="agency.min.js"></script>
