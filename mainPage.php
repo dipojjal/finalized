5ed076489071266163e8c600317ba83f7f4a36ab
dipojjal <cooldipojjal@gmail.com>
2017-07-11 18:16:51 +0530

    mainPage

--- mainPage.php
+++ mainPage.php
@@ -66,58 +66,7 @@
     <script src="contact_me.js"></script>		
 
     <!-- Navigation -->
-    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse" id="mainNav">
-        <div class="container">
-            <img src="img/logos/Wipro_Logo_RGB.png" height="80px" width="110px"/>
-
-
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu <i class="fa fa-bars"></i>
-            </button>
-            
-            <a class="navbar-brand" href="#page-top">WIPRO TECHNOLOGIES</a>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                	<li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#features">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#portfolio">Menu</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">Process</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#team">Team</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#help">Help</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
-                    </li>
-		    <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Header -->
-    <header class="masthead">
-        <div class="container">
-            <div class="intro-text">
-                <div class="intro-lead-in">Wipro Product Management System</div>
-                <div class="intro-heading"></div>
-                <a href="#portfolio" class="btn btn-xl">Main Menu</a>
-            </div>
-        </div>
-    </header>
-
+<?php include 'header.php'?>
     <!-- Services -->
     <section id="features">
         <div class="container">
