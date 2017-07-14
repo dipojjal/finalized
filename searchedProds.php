a52668c1adfd6a26ea7fe422d4d279db9a12159c
dipojjal <cooldipojjal@gmail.com>
2017-07-14 19:03:09 +0530

    searchedProds.php

--- searchedProds.php
+++ searchedProds.php
@@ -7,14 +7,12 @@
 <body>
 
 <?php
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
-}
-$varSearchedItem= $_POST['search'];
+
+  include 'connection.php';
+  
+  $varSearchedItem= $_POST['search'];
   $searchTerm= '%'.$varSearchedItem.'%';
   
-  $pg_conn = pg_connect(pg_connection_string_from_database_url());
   $searchedProds = pg_query($pg_conn, "SELECT Id, sfid, Name, productcode, isactive, country__c, state__c, description FROM salesforce.Product2 where (Name like '$searchTerm' OR productcode like '$searchTerm')");
   echo"<div class=\"panel panel-default\">";
   
