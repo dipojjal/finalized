d3e3dbdafe449ee7c1e530cc837ac2d7b2642e24
dipojjal <cooldipojjal@gmail.com>
2017-07-14 18:30:06 +0530

    connection

--- createprod.php
+++ createprod.php
@@ -7,10 +7,9 @@
 <body>
 
 <?php
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
-}
+
+  include 'connection.php';
+  
   $name= $_POST['name'];
   $productCode= $_POST['prodcode'];
   $productFamily= $_POST['family'];
@@ -19,7 +18,6 @@ function pg_connection_string_from_database_url() {
   $productState= $_POST['state'];
   $productDescription= $_POST['description'];
   
-  $pg_conn = pg_connect(pg_connection_string_from_database_url());
   $query= "insert into salesforce.Product2 (Name, productcode, family, isactive, country__c, state__c, description) Values ('$name', '$productCode', '$productFamily', '$isActive', '$productCountry', '$productState', '$productDescription');";
   
   $createProd = pg_query($pg_conn, $query);
