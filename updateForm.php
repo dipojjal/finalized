9ec8678a3cece5f7aa4e360d50a071a2094f8485
dipojjal <cooldipojjal@gmail.com>
2017-07-16 12:21:48 +0530

    update

--- updateForm.php
+++ updateForm.php
@@ -1,5 +1,9 @@
 <?php
 
-include 'connection.php';
-
+  include 'connection.php';
+  
+  $Id= $_POST['Id'];
+  
+  $searchedProds = pg_query($pg_conn, "DELETE FROM salesforce.Product2 WHERE Id='$Id'");
+  
 ?>
