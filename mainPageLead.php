32718bafa315e0d2c4f9655fc947dc082fbdb6dc
dipojjal <cooldipojjal@gmail.com>
2017-07-20 13:41:47 +0530

    mainPageLead

--- mainPageLead.php
+++ mainPageLead.php
@@ -850,7 +850,8 @@
 <div style="clear:both; font-size=60px">Hello, php starts here</div>
 <?php
 include('connection.php'); //connect				    
- $result = pg_query($pg_conn, "select Id, sfid, name, firstname, lastname, email,  status, city, state, company, country, CreatedDate, description, Push_To_Heroku__c FROM salesforce.lead_backup_new;");
+ $result = pg_query($pg_conn, "select Id, sfid, street, numberofemployees, name, firstname, lastname, email,  status, city, state, company, country, CreatedDate, description, Push_To_Heroku__c FROM salesforce.salesforce.leads_archive;");
+
 if (!$result) {
   echo "An error occurred.\n";
   exit;
