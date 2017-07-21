865f76d30117d1d3cc4c14da31539967cf7b7aa3
dipojjal <cooldipojjal@gmail.com>
2017-07-21 13:43:18 +0530

    testScript

--- testScript.php
+++ testScript.php
@@ -6,10 +6,8 @@ $fo=fopen("Test.json","r");
 $fr= fread($fo, filesize("Test.json"));
 
 $array= json_decode($fr,true);
-
 $query="insert into testTable values ('$array[ID]','$array[NAME]')";
 
-
-
+$result = pg_query($pg_conn, '$query');
 
 ?>
